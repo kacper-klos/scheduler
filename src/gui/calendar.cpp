31e03b4331f9d498f253d69a3f7fe3bf8592af1c
kacper16621 <kacper.klos@proton.me>
2025-09-06 17:40:17 +0200

    Change a structure of calendar table, detached the data from column and row header

--- src/gui/calendar.cpp
+++ src/gui/calendar.cpp
@@ -5,41 +5,49 @@
 Calendar::Calendar(uint8_t hour_start, uint8_t hour_end, QObject *parent) : QAbstractTableModel(parent) {
     assert(hour_end >= hour_start && hour_end <= 24 && hour_start <= 24);
 
-    int rows_count = (hour_end - hour_start + 1) + 1;
+    int rows_count = (hour_end - hour_start + 1);
 
-    QVector<QVector<QString>> data(rows_count);
-    QVector<QString> header = {QString("Godzina"), QString("Ponidziałek"), QString("Wtorek"),
-                               QString("Środa"),   QString("Czwartek"),    QString("Piątek")};
-    data[0] = header;
+    // Column header
+    this->column_header_ = {QString("Godzina"), QString("Ponidziałek"), QString("Wtorek"),
+                            QString("Środa"),   QString("Czwartek"),    QString("Piątek")};
 
-    for (int i = 1; i < rows_count; ++i) {
-        QVector<QString> row(WORK_DAYS_IN_WEEK_AND_HEADER);
-        row[0] = QString::fromStdString(std::to_string(hour_start - 1 + i) + ":00");
-        data[i] = row;
+    // Column with time
+    QVector<QString> time_column(rows_count);
+    for (int i = 0; i < rows_count; ++i) {
+        time_column[i] = QString::fromStdString(std::to_string(hour_start + i) + ":00");
     }
+    this->time_column_ = time_column;
+
+    // Data vectors
+    QVector<QVector<QString>> data(WORK_DAYS_IN_WEEK);
+    for (int i = 0; i < WORK_DAYS_IN_WEEK; ++i) {
+        QVector<QString> column(rows_count);
+        data[i] = column;
+    }
+
     this->data_ = data;
 }
 
-int Calendar::rowCount(const QModelIndex &parent) const { return static_cast<int>(data_.size()); }
+int Calendar::rowCount(const QModelIndex &parent) const { return static_cast<int>(this->time_column_.size()) + 1; }
 
 int Calendar::columnCount(const QModelIndex &parent) const { return WORK_DAYS_IN_WEEK_AND_HEADER; }
 
 QVariant Calendar::data(const QModelIndex &index, int role) const {
     assert(index.isValid() && index.row() < this->rowCount() && index.column() < this->columnCount());
-    return this->data_[index.row()][index.column()];
-}
-
-QVariant Calendar::headerData(int section, Qt::Orientation orientation, int role) const {
-    if (Qt::Vertical) {
-        assert(section < this->rowCount());
-        return this->data_[section];
-    } else {
-        assert(section < this->columnCount());
-        int row_count = this->rowCount();
-        QList<QString> column(row_count);
-        for (int i = 0; i < row_count; ++i) {
-            column[i] = data_[i][section];
+    if (role == Qt::DisplayRole) {
+        // Column header
+        if (index.row() == 0) {
+            return this->column_header_[index.column()];
         }
-        return column;
+        // Time row
+        else if (index.column() == 0) {
+            return this->time_column_[index.row() - 1];
+        }
+        // Data
+        else {
+            return this->data_[index.column() - 1][index.row() - 1];
+        }
     }
+
+    return {};
 }
